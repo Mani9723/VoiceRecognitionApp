cf2451346231bd194d235c154700caed556873ba
Mani9732 <mani.shah23@gmail.com>
2020-11-27 15:14:10 -0500

    First layout done, activities added

---
+++ app/src/main/java/com/cs477/courseproject_mshah22/CalendarActivity.java
@@ -0,0 +1,14 @@
+package com.cs477.courseproject_mshah22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
