2b225d303839a4c2513c81e753d88ebbe001af74
Mani9732 <mani.shah23@gmail.com>
2020-11-27 20:29:00 -0500

    Basic reminder flow completed

--- app/src/main/java/com/cs477/courseproject_mshah22/VoiceCommandActivity.java
+++ app/src/main/java/com/cs477/courseproject_mshah22/VoiceCommandActivity.java
@@ -6,18 +6,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
 public class VoiceCommandActivity extends AppCompatActivity {
     private static final int SPEECH_REQUEST_CODE = 0;
 
+    private TextView introTextView;
+    private String reminderDescription;
+    private String reminderDay;
+    private String reminderTime;
+    private int currentStep;
+    private boolean remindersMode;
+    private boolean calendarMode;
+
+    private Reminder reminder;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voice_command);
+        introTextView = (TextView)findViewById(R.id.commandsInstructionView);
+        currentStep = 0;
     }
 
     // Create an intent that can start the Speech Recognizer activity
@@ -39,9 +56,100 @@ public class VoiceCommandActivity extends AppCompatActivity {
                     RecognizerIntent.EXTRA_RESULTS);
             String spokenText = results.get(0);
             System.out.println(spokenText);
-            // Do something with spokenText
+
             Toast.makeText(getApplicationContext(),spokenText,Toast.LENGTH_SHORT).show();
+            if(spokenText.equalsIgnoreCase("Create Reminder")){
+                remindersMode = true;
+                calendarMode = false;
+                currentStep++;
+                introTextView.setText(R.string.reminderStep1);
+            }else if(spokenText.equalsIgnoreCase("Create Calendar Event")){
+                calendarMode = true;
+                remindersMode = false;
+                currentStep++;
+            }else if(spokenText.equalsIgnoreCase("Next")){
+                currentStep = 1;
+                if(remindersMode){
+                    remindersMode = true;
+                    introTextView.setText(R.string.reminderStep1);
+                }else if(calendarMode){
+                    calendarMode = true;
+                }
+            }else if(spokenText.equalsIgnoreCase("Exit")){
+                remindersMode = false;
+                calendarMode = false;
+                currentStep = 0;
+                introTextView.setText(R.string.commandsIntro);
+                Reminder reminder = new Reminder(reminderDescription,reminderDay,reminderTime);
+                System.out.println(reminder);
+
+            }else {
+                if (remindersMode) {
+                    processReminders(spokenText);
+                }
+                if (calendarMode) {
+                    processCalendarEvent(spokenText);
+                }
+            }
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void processReminders(String input)
+    {
+        if(currentStep == 1){
+            introTextView.setText(R.string.reminderStep2);
+            System.out.println("Description: " +input);
+            reminderDescription = input;
+        }else if(currentStep == 2){
+            introTextView.setText(R.string.reminderStep3);
+            reminderDay = input;
+            System.out.println("Date: " +input);
+        }else if(currentStep == 3){
+            introTextView.setText(R.string.reminderStep4);
+            reminderTime = input;
+            System.out.println("Time: " +input);
+        }else if(currentStep == 4){
+            currentStep = 0;
+        }
+        currentStep++;
+    }
+    private void processCalendarEvent(String input)
+    {
+
+    }
+
+
+    private class Reminder {
+
+        private String desc, date, time;
+
+        public Reminder(String desc, String date, String time)
+        {
+            this.desc = desc;
+            this.date = date;
+            this.time = time;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        @Override
+        public String toString() {
+            return "Reminder{" +
+                    "desc='" + desc + '\'' +
+                    ", date='" + date + '\'' +
+                    ", time='" + time + '\'' +
+                    '}';
+        }
     }
 }
