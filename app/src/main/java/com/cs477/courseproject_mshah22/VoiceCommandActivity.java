2a9a903761e392924e546fa7b4c04396206dd365
Mani9732 <mani.shah23@gmail.com>
2020-12-08 20:15:19 -0500

    95% done

--- app/src/main/java/com/cs477/courseproject_mshah22/VoiceCommandActivity.java
+++ app/src/main/java/com/cs477/courseproject_mshah22/VoiceCommandActivity.java
@@ -1,12 +1,19 @@
 package com.cs477.courseproject_mshah22;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.CalendarContract;
 import android.provider.CalendarContract.Events;
+import android.provider.CalendarContract.Reminders;
 import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.TextView;
@@ -17,19 +24,15 @@ import java.util.List;
 
 public class VoiceCommandActivity extends AppCompatActivity {
     private static final int SPEECH_REQUEST_CODE = 0;
+    private static int NOTIFICATION_ID = 1;
 
     private TextView introTextView;
-    private String reminderDescription, calendarDescription;
-    private String reminderDay, availability, calendarTitle;
-    private String reminderTime, calendarEventLocation;
+    private String calendarDescription;
+    private String availability, calendarTitle;
+    private String calendarEventLocation;
     private int currentStep;
     private boolean remindersMode;
     private boolean calendarMode;
-
-    private Reminder reminder;
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,44 +40,15 @@ public class VoiceCommandActivity extends AppCompatActivity {
         setContentView(R.layout.activity_voice_command);
         introTextView = (TextView)findViewById(R.id.commandsInstructionView);
         currentStep = 0;
-
-        //addToCalendar();
     }
 
-    public void addToCalendar(CalendarEvent event)
-    {
-        int avail = Events.AVAILABILITY_FREE;
-        if(event.getAvailability().equalsIgnoreCase("busy")){
-            avail = Events.AVAILABILITY_BUSY;
-        }
-        Calendar beginTime = Calendar.getInstance();
-        beginTime.set(2020, 0, 19, 7, 30);
-        Calendar endTime = Calendar.getInstance();
-        endTime.set(2020, 0, 19, 8, 30);
-        Intent intent = new Intent(Intent.ACTION_INSERT)
-                .setData(Events.CONTENT_URI)
-                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis())
-                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis())
-                .putExtra(Events.TITLE, event.getTitle())
-                .putExtra(Events.DESCRIPTION, event.getEventDescript())
-                .putExtra(Events.EVENT_LOCATION, event.getLocation())
-                .putExtra(Events.AVAILABILITY, avail);
-        startActivity(intent);
-
-
-    }
-
-    // Create an intent that can start the Speech Recognizer activity
     public void displaySpeechRecognizer(View view) {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-// Start the activity, the intent will be populated with the speech text
         startActivityForResult(intent, SPEECH_REQUEST_CODE);
     }
 
-    // This callback is invoked when the Speech Recognizer returns.
-// This is where you process the intent and extract the speech text from the intent.
     @Override
     protected void onActivityResult(int requestCode, int resultCode,
                                     Intent data) {
@@ -108,9 +82,6 @@ public class VoiceCommandActivity extends AppCompatActivity {
                 calendarMode = false;
                 currentStep = 0;
                 introTextView.setText(R.string.commandsIntro);
-                Reminder reminder = new Reminder(reminderDescription,reminderDay,reminderTime);
-                System.out.println(reminder);
-
             }else if(remindersMode || calendarMode){
                 if (remindersMode) {
                     processReminders(spokenText);
@@ -130,18 +101,9 @@ public class VoiceCommandActivity extends AppCompatActivity {
     private void processReminders(String input)
     {
         if(currentStep == 1){
-            introTextView.setText(R.string.reminderStep2);
-            System.out.println("Description: " +input);
-            reminderDescription = input;
-        }else if(currentStep == 2){
-            introTextView.setText(R.string.reminderStep3);
-            reminderDay = input;
-            System.out.println("Date: " +input);
-        }else if(currentStep == 3){
             introTextView.setText(R.string.reminderStep4);
-            reminderTime = input;
-            System.out.println("Time: " +input);
-        }else if(currentStep == 4){
+            finalizeReminder(input);
+        }else if(currentStep == 2){
             currentStep = 0;
         }
         currentStep++;
@@ -173,43 +135,42 @@ public class VoiceCommandActivity extends AppCompatActivity {
         currentStep++;
     }
 
+    private void finalizeReminder(String reminder)
+    {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext())
+                .setSmallIcon(R.drawable.ic_small)
+                .setContentTitle("Reminder")
+                .setContentText(reminder)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+        NOTIFICATION_ID++;
+    }
+
     private void finalizeToCalendar(CalendarEvent event)
     {
         addToCalendar(event);
     }
 
-
-    private static class Reminder {
-
-        private String desc, date, time;
-
-        public Reminder(String desc, String date, String time)
-        {
-            this.desc = desc;
-            this.date = date;
-            this.time = time;
+    public void addToCalendar(CalendarEvent event)
+    {
+        int avail = Events.AVAILABILITY_FREE;
+        if(event.getAvailability().equalsIgnoreCase("busy")){
+            avail = Events.AVAILABILITY_BUSY;
         }
-
-        public String getDesc() {
-            return desc;
-        }
-
-        public String getDate() {
-            return date;
-        }
-
-        public String getTime() {
-            return time;
-        }
-
-        @Override
-        public String toString() {
-            return "Reminder{" +
-                    "desc='" + desc + '\'' +
-                    ", date='" + date + '\'' +
-                    ", time='" + time + '\'' +
-                    '}';
-        }
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(2020, 0, 19, 7, 30);
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2020, 0, 19, 8, 30);
+        Intent intent = new Intent(Intent.ACTION_INSERT)
+                .setData(Events.CONTENT_URI)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis())
+                .putExtra(Events.TITLE, event.getTitle())
+                .putExtra(Events.DESCRIPTION, event.getEventDescript())
+                .putExtra(Events.EVENT_LOCATION, event.getLocation())
+                .putExtra(Events.AVAILABILITY, avail);
+        startActivity(intent);
     }
 
     private static class CalendarEvent {
